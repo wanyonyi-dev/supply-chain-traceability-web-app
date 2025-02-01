e0595dd718029bbf26cd50b759c09230c4063ec3
wanyonyi-dev <wanyonyip148@gmail.com>
2025-02-01 17:05:44 +0300

    changes

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   webpack: {
     configure: {
-      ignoreWarnings: [
-        function ignoreSourcemapsloaderWarnings(warning) {
-          return (
-            warning.module &&
-            warning.module.resource.includes('node_modules') &&
-            warning.details &&
-            warning.details.includes('source-map-loader')
-          );
-        },
-      ],
+      resolve: {
+        fallback: {
+          "crypto": require.resolve("crypto-browserify"),
+          "stream": require.resolve("stream-browserify"),
+          "assert": require.resolve("assert"),
+          "http": require.resolve("stream-http"),
+          "https": require.resolve("https-browserify"),
+          "os": require.resolve("os-browserify"),
+          "url": require.resolve("url")
+        }
+      }
     },
   },
 }; 
