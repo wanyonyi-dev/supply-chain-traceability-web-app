2bdfd432203fb91285a4b8dcaccf7ce2d75e3d56
wanyonyi-dev <wanyonyip148@gmail.com>
2025-02-01 17:34:21 +0300

    new

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,26 @@
+const webpack = require('webpack');
+
 module.exports = {
   webpack: {
-    configure: {
-      resolve: {
-        fallback: {
-          "crypto": require.resolve("crypto-browserify"),
-          "stream": require.resolve("stream-browserify"),
-          "assert": require.resolve("assert"),
-          "http": require.resolve("stream-http"),
-          "https": require.resolve("https-browserify"),
-          "os": require.resolve("os-browserify"),
-          "url": require.resolve("url")
-        }
-      }
-    },
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify'),
+        url: require.resolve('url')
+      };
+
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer']
+        })
+      );
+
+      return webpackConfig;
+    }
   },
 }; 
